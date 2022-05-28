dce64bc0acfab9ea54887939a1582f47b71de2b2
Pau Siquier <100527210+PauFish@users.noreply.github.com>
2022-05-28 10:11:45 +0200

    Pequeños codigos en JS
    Pequeñas practicas que voy realizando

---
+++ VT04 DWEC - Ejemplos/ejemplo2.js
@@ -0,0 +1,18 @@
+// Crea una lista con todos los elementos td.
+var listaDeValores = document.getElementsByTagName('td') 
+
+var resultado = 0;
+
+for (var i = 0; i < listaDeValores.length; i++) {
+	// Imprime todos los elementos td
+	console.log(listaDeValores[i]); 
+
+	// Convierte el contenido de cada td en entero y los va sumando. Prueba a quitar el parseInt.
+	resultado = resultado + parseInt(listaDeValores[i].innerHTML); 
+	// resultado = resultado + listaDeValores[i].innerHTML;
+}
+
+// Imprime el resultado de la suma, lo convierte a cadena de caracteres y nos devuelve cuantos caracteres tiene el resultado.
+console.log("El resultado final es " + resultado + " y tiene " + String(resultado).length + " caracteres."); 
+
+
---
+++ VT04 DWEC - Ejemplos/ejemplo5.js
@@ -0,0 +1,10 @@
+listaA = ["alegria", "esfuerzo", "detalles", "ilusion"];
+listaB = ["Lunes", "Martes", "Miercoles", "Jueves", "Viernes"];
+
+listaDeListas = [listaA, listaB]
+
+console.log(listaA[1]); // Elemento en la posición 1 de la listaA
+console.log(listaB[3]); // Elemento en la posición 3 de la listaB
+console.log(listaDeListas[0][1]); // Elemento en la posición 1 de la lista que está en la posición 0 de listaDeListas
+console.log(listaDeListas[1][3]); // Elemento en la posición 3 de la lista que está en la posición 1 de listaDeListas
+console.log(listaDeListas[1][0]); // Elemento en la posición 0 de la lista que está en la posición 1 de listaDeListas
