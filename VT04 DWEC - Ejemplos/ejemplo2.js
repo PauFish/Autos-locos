1b9dacd2ae0834448abfbf8f8dbfc04434c8c780
Pau Siquier <100527210+PauFish@users.noreply.github.com>
2022-06-04 23:12:36 +0200

    Add files via upload

--- VT04 DWEC - Ejemplos/ejemplo2.js
+++ VT04 DWEC - Ejemplos/ejemplo2.js
@@ -1,44 +1,43 @@
-function ejecuta() {
-	var a = "10";
-	var b = 10;
-	var c = 10.0;
+// Mostramos la fecha actual
+var fechaActual = new Date(Date.now()); // Creamos un objeto tipo Date y lo inicializamos con el momento actual
 
-	console.log ("Compruebo si 10 es igual a 10.0")
-	if (b == c) {
-		console.log("Son iguales");
-	} else {
-		console.log("Son distintos")
+document.getElementById('fecha').innerHTML = fechaActual.getDate() + " de " + 
+	fechaActual.toLocaleString("es-ES", {month: "long"}) + " de " + /* muestra el mes en el idioma */
+	fechaActual.getFullYear(); 
+
+
+// Mostramos el segundo actual
+var segundoActual = fechaActual.getSeconds();
+
+document.getElementById("segundos").innerHTML = segundoActual;
+
+
+// Utilizamos Math para multiplicar el segundo actual por PI y redondear el valor.
+var resultado = Math.round(segundoActual * Math.PI);
+
+document.getElementById("resultado").innerHTML = resultado;
+
+
+// Variable que cambiará el color de fondo
+var color;
+
+
+// La función cambia el valor de la variable dependiendo de su paridad y devuelve un texto.
+function parOImpar(valor) {
+	if (valor % 2 == 0) {
+		color = "red";
+		return "par";
 	}
-
-	// Enteros y Float son de tipo Number
-
-	if (b === c) {
-		console.log("Son iguales y del mismo tipo");
-	} else {
-		console.log("Son iguales pero de otro tipo")
+	else {
+		color = "blue";
+		return "impar";
 	}
-
-
-	// Un bucle que añade a "a" un "0" por cada valor que tenga "b"
-	while (b > 0) {
-		a = a + "0";
-		b = b - 1;
-		console.log(a);
-	}
-
-	// Un switch nos permite controlar diferentes condiciones
-	switch(c) {
-		case 10:
-			console.log("Vale 10"); // Sin el break, entra en el siguiente caso
-		case 9:
-			console.log("Vale 9");
-			break; // El break nos saca del switch
-		case 8:
-			console.log("Vale 8");
-			break
-		default: // Para cualquier otro caso
-			console.log("Es otra cosa");	
-	}
-
 }
+
+
+// Mostramos el texto de la paridad y cambiamos el color de fondo.
+document.getElementById("paridad").innerHTML = parOImpar(resultado);
+document.getElementsByTagName('body')[0].style.backgroundColor = color; // Equivale a la propiedad background-color de CSS.
+
+
 
